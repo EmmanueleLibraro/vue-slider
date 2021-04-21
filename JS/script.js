d8dae30c8dbf33ca6ef6a664ef839c13d1908f6a
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-21 18:10:26 +0200

    fine

--- JS/script.js
+++ JS/script.js
@@ -8,25 +8,41 @@ const app = new Vue({
             './img-slider/Settlement_01.jpg',
         ],
         indexFoto: 0,     //INDICE INIZIALE DELL'ARRAY
+        interval: 0,     //INTERVALLO PER FERMARE LE FOTO
         
     },
-
+    created(){         //PAROLA CHIAVE PER CREARE ISTANZA VUE 
+        this.startLoop();
+    },
     methods: {
         //FUNZIONI PER FECCIA DESTRA E SINISTRA
         prevFoto(){
-            this.indexFoto --;    //DIMINUIAMO IL VALORE CON ' - - '
+            this.indexFoto --;    //DIMINUIAMO IL VALORE DELL'INDEX CON ' - - '
 
             if(this.indexFoto < 0){
                 this.indexFoto = this.foto.length - 1;
             }
         },
         nextFoto(){
-            this.indexFoto ++;    //INCREMENTIAMO IL VALORE CON ' + + '
+            this.indexFoto ++;    //INCREMENTIAMO IL VALORE DELL'INDEX CON ' + + '
 
             if(this.indexFoto > (this.foto.length) -1 ){       //SERVE PER RITORNARE ALLA FOTO INIZIALE
                 this.indexFoto = 0;
             }
+        },
+        setFoto(index){   //INDICE CLICK DEI PALLINI CHE SI RIFERISCE ALL'INDEX DELLE FOTO
+            this.indexFoto = index;
+        },
+        startLoop(){
+            this.interval= setInterval(() => {
+                this.nextFoto();        //OGNI 3 SEC LE IMG CAMBIANO DA SOLE
+            }, 3000);
+        },
+        stopLoop(){
+           clearInterval(this.interval);   //FERMA L'AVANZARE DELLE FOTO
         }
+        
+
     }
 });
 
