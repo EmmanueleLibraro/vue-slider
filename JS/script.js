c76884e7e4222fbdd26568dcfc4d8b0e2525ac5f
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-21 16:49:31 +0200

    errore img

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        foto: [
+            './img-slider/81ICUia+GoL._AC_SL1500_.jpg',
+            './img-slider/header---image.jpg',
+            './img-slider/New-World-TGA-Shot-06.jpg',
+            './img-slider/Settlement_01.jpg',
+        ],
+        
+    }
+});
